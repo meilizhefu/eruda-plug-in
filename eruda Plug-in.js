331da02e025b374e0f762459c87eb7d39e02d889
meilizhefu <52991097+meilizhefu@users.noreply.github.com>
2019-07-17 15:44:46 +0800

    Update eruda Plug-in.js

--- eruda Plug-in.js
+++ eruda Plug-in.js
@@ -8,19 +8,21 @@
 // @grant        none
 // ==/UserScript==
 
-
 document.onmousedown=function(ev){
 setTimeout(function (){
 
 var as = document.getElementsByTagName("h2");
 for(var i = 0; i < as.length; i++) {  
 var txt = as[i].innerText || as[i].textContent; 
+ 
 if(txt === "Attributes") 
 { as[i].setAttribute('class',"ysbq-sx");}
 
 if(txt === "Styles") 
 { as[i].setAttribute('class',"ysbq-ys");}
 
+ if(txt === "Event Listeners") 
+{ as[i].setAttribute('class',"ysbq-sjjk");}
  }
 
 var hhbys = document.getElementsByClassName("ysbq-sx")[0];
@@ -32,5 +34,11 @@ hhbys.innerHTML="加载样式"
 var hhbys = document.getElementsByClassName("ysbq-ys")[0];
 hhbys.innerHTML="样式"
 
+var hhbys = document.getElementsByClassName("ysbq-sjjk")[0];
+hhbys.innerHTML="事件监控"
 }, 100);
 }
+
+
+
+
