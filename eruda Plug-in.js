3d2f0e4ffea454014088187419b0b5ffa338eb8d
meilizhefu <52991097+meilizhefu@users.noreply.github.com>
2019-07-18 12:37:04 +0800

    Update eruda Plug-in.js

--- eruda Plug-in.js
+++ eruda Plug-in.js
@@ -1,13 +1,21 @@
 // ==UserScript==
-// @name        eruda Plug-in
+// @name        测试当前1.3.0
 // @namespace    http://your.homepage/
 // @version      0.1
 // @description  enter something useful
 // @author       You
-// @match        https://github.com/meilizhefu/eruda-plug-in
+// @match        https://www.yunpanjingling.com/
 // @grant        none
 // ==/UserScript==
 
+var hm = document.createElement("script");
+hm.src = "https://cdn.bootcss.com/eruda/1.3.0/eruda.min.js";
+hm.onload=function(){eruda.init()};
+var s = document.getElementsByTagName("title")[0]; 
+s.parentNode.insertBefore(hm, s);
+
+
+//下面是汉化项目
 document.onmousedown=function(ev){
 setTimeout(function (){
 
@@ -16,29 +24,101 @@ for(var i = 0; i < as.length; i++) {
 var txt = as[i].innerText || as[i].textContent; 
  
 if(txt === "Attributes") 
-{ as[i].setAttribute('class',"ysbq-sx");}
-
-if(txt === "Styles") 
-{ as[i].setAttribute('class',"ysbq-ys");}
-
- if(txt === "Event Listeners") 
-{ as[i].setAttribute('class',"ysbq-sjjk");}
- }
-
+{ as[i].setAttribute('class',"ysbq-sx");
 var hhbys = document.getElementsByClassName("ysbq-sx")[0];
-hhbys.innerHTML="属性"
-
+     hhbys.innerHTML="属性" ;
+ 
 var hhbys = document.getElementsByClassName("eruda-toggle-all-computed-style eruda-active-effect")[0];
-hhbys.innerHTML="加载样式"
-
-var hhbys = document.getElementsByClassName("ysbq-ys")[0];
-hhbys.innerHTML="样式"
-
-var hhbys = document.getElementsByClassName("ysbq-sjjk")[0];
-hhbys.innerHTML="事件监控"
-}, 100);
+     hhbys.innerHTML="加载样式" ; 
 }
 
+    
+if(txt === "Styles") 
+{ as[i].setAttribute('class',"ysbq-ys");
+var hhbys = document.getElementsByClassName("ysbq-ys")[0];
+     hhbys.innerHTML="样式" ;
+}
+
+    
+ if(txt === "Event Listeners") 
+{ as[i].setAttribute('class',"ysbq-sjjk");
+var hhbys = document.getElementsByClassName("ysbq-sjjk")[0];
+    hhbys.innerHTML="事件监控"
+}
+    
+
+  
+ }
+
+    
 
 
 
+
+
+
+
+//下面是点击复制class的功能
+var as = document.getElementsByTagName("td");
+for(var i = 0; i < as.length; i++) {  
+var txt = as[i].innerText || as[i].textContent; 
+if(txt === "class") 
+{ as[i+1].setAttribute('class',"cslass");
+
+var btn = document.getElementsByClassName('cslass');
+    btn[0].addEventListener('click', function(){
+
+//创建input标签
+var hm = document.createElement("input");
+hm.id = "fzwb";
+hm.type = "text";
+hm.value = document.getElementsByClassName("cslass")[0].innerHTML;
+hm.style = "opacity:0";
+var s = document.getElementById("eruda"); 
+s.parentNode.insertBefore(hm, s)
+hm.focus();
+hm.setSelectionRange(0, hm.value.length);
+var res = document.execCommand('copy', true);
+alert(hm.value)
+
+var obj=document.getElementById("fzwb");
+var parentObj=obj.parentNode;
+parentObj.removeChild(obj);
+
+        
+  });
+
+
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}, 100);
+}
